6f2fe9157ab2330203bf9c068ed6cd2fbd53e184
Fadli Muharram <fadli@leson.id>
2020-09-08 09:54:06 +0700

    initial

---
+++ server/controllers/WhatsappBot.js
@@ -0,0 +1,43 @@
+import dotenv from 'dotenv';
+import twilio from 'twilio';
+dotenv.config();
+
+const { SID: accountSid, KEY: TwilloAuthToken } = process.env;
+
+twilio(accountSid, TwilloAuthToken);
+const { MessagingResponse } = twilio.twiml;
+
+class WhatsappBot {
+	static async exampleMsg(req, res, next) {
+		const twiml = new MessagingResponse();
+		const q = req.body.Body;
+		const selectedMsg = req.session.pilihan;
+
+		try {
+			let msg = '';
+			req.session.pilihan = q;
+
+			if (!selectedMsg && q === 'hi') {
+				msg = '[1] untuk melihat jam [2] untuk melihat hari [3] untuk melihat bulan';
+			} else {
+				if (q === '1') {
+					msg = 'jam';
+				} else if (q === '2') {
+					msg = 'hari';
+				} else if (q === '3') {
+					msg = 'bulan';
+				}
+			}
+
+			twiml.message(msg);
+
+			res.set('Content-Type', 'text/xml');
+
+			return res.status(200).send(twiml.toString());
+		} catch (error) {
+			return next(error);
+		}
+	}
+}
+
+export default WhatsappBot;
